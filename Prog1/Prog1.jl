5c2b18ced80176b383bb650f0fe805638ee00ebd
J. A. Farid <jrab227@gmail.com>
2017-10-02 22:36:22 -0400

    Updating tests

--- Prog1/Prog1.jl
+++ Prog1/Prog1.jl
@@ -4,23 +4,23 @@ module Prog1
     Main struct for the set object
   =#
   mutable struct MySet
-    small::Array{Integer, 1}
-    large::Array{Integer, 1}
-    sizeSmall::Integer
-    sizeLarge::Integer
+    small::Array{Int64, 1}
+    large::Array{Int64, 1}
+    sizeSmall::Int64
+    sizeLarge::Int64
   end
 
   #=
     Searches for elem in the smaller set, then larger set using binary search.
   =#
-  function search(set::MySet, elem::Integer)::Bool
+  function search(set::MySet, elem::Int64)::Bool
     return binarySearch(set.small, set.sizeSmall, elem) || 
       binarySearch(set.large, set.sizeLarge, elem); 
   end
   
-  function binarySearch(arr::Array{Integer, 1}, size::Integer, elem::Integer)::Bool
+  function binarySearch(arr::Array{Int64, 1}, size::Int64, elem::Int64)::Bool
     k = (1, size);
-    m::Integer = 0;
+    m::Int64 = 0;
     while (k[1] < k[2])
       m = k[1] + floor((k[2] - k[1])/2)
       if (arr[m] < elem)
@@ -36,11 +36,12 @@ module Prog1
     Inserts element into a MySet instance, inserting into the smaller array
     instance, or merging with the larger array instance.
   =#
-  function insert(set::MySet, elem::Integer)::MySet
+  function insert(set::MySet, elem::Int64)::MySet
     if (set.sizeSmall + 1 == length(set.small))
       newSmall = insertArray(set.small, set.sizeSmall + 1, elem);
       set.large = mergeArrays(set.large, newSmall, set.sizeLarge);
-      set.small = [-1 for k=1:length(set.small)];
+      Infi::Int64 = (2^63) - 1;           # Int64 infinity
+      set.small::Array{Int64, 1} = [ Infi for i=1:length(set.small) ];
       set.sizeSmall = 0;
     else
       set.small = insertArray(set.small, set.sizeSmall + 1, elem);
@@ -53,8 +54,8 @@ module Prog1
     Takes a sorted array arr, and inserts i first in position n, then
     places i in the correct sorted position using insertion sorting.
   =#
-  function insertArray(arr::Array{Integer, 1},
-    i::Integer, elem::Integer)::Array{Integer, 1}
+  function insertArray(arr::Array{Int64, 1},
+    i::Int64, elem::Int64)::Array{Int64, 1}
 
     curr = i;
     arr[i] = elem;
@@ -74,10 +75,10 @@ module Prog1
     in the large array currently. If the merge has more elements than the large array
     can hold, an index out of bounds error is thrown.
   =#
-  function mergeArrays(largeArr::Array{Integer,1},
-    smallArr::Array{Integer,1}, sizeLarge::Integer)::Array{Integer, 1}
+  function mergeArrays(largeArr::Array{Int64,1},
+    smallArr::Array{Int64,1}, sizeLarge::Int64)::Array{Int64, 1}
 
-    newArray = Array{Integer,1}(length(largeArr));
+    newArray = Array{Int64,1}(length(largeArr));
 
     if (length(largeArr) < length(smallArr) + sizeLarge)
       throw(BoundsError("Combined new length is larger than available size"));
@@ -89,7 +90,6 @@ module Prog1
     i = 1; # Index for current value of new array
 
     newSize = sizeLarge + length(smallArr);
-
     while(i <= newSize)
       if (largeArr[k] < smallArr[j])
         newArray[i] = largeArr[k];
@@ -109,7 +109,6 @@ module Prog1
           k += 1;
         end
       end
-
     end
     return newArray;
   end
--- Prog1/test.jl
+++ Prog1/test.jl
@@ -3,21 +3,40 @@ module Prog1Test
   include("Prog1.jl");
   using Base.Test;
 
+  function testSequence()
+    n = 10^2
+    Infi::Int64 = (2^63) - 1;           # Int64 infinity
+    short = [ Infi for i=1:sqrt(n)];
+    large = [ Infi for i=1:n];
+    set = Prog1.MySet(short, large, 0, 0);
+    for i=1:11
+      set = Prog1.insert(set, 12-i);
+    end
+
+    for i=1:10
+      @test set.large[i] == i+1;
+    end
+
+    @test set.small[1] == 1;
+
+    
+  end
+
   # Test for merge arrays
   function mergeArray()
 
     # Merge case 1
-    k1::Array{Integer, 1} = [1, 5, 6, 7, 8, -1, -1, -1];
-    j1::Array{Integer, 1} = [2, 3, 4];
-    result1:: Array{Integer, 1} = Prog1.mergeArrays(k1, j1, 5);
+    k1::Array{Int64, 1} = [1, 5, 6, 7, 8, -1, -1, -1];
+    j1::Array{Int64, 1} = [2, 3, 4];
+    result1:: Array{Int64, 1} = Prog1.mergeArrays(k1, j1, 5);
     for i = 1:8
       @test result1[i] === i;
     end
 
     # Merge case 2
-    k2::Array{Integer, 1} = [5, 6, -1, -1, -1, -1, -1];
-    j2::Array{Integer, 1} = [2, 3, 4];
-    result2:: Array{Integer, 1} = Prog1.mergeArrays(k2, j2, 2);
+    k2::Array{Int64, 1} = [5, 6, -1, -1, -1, -1, -1];
+    j2::Array{Int64, 1} = [2, 3, 4];
+    result2:: Array{Int64, 1} = Prog1.mergeArrays(k2, j2, 2);
     for i = 2:6
       @test result2[i-1] === i;
     end
@@ -25,15 +44,15 @@ module Prog1Test
     #=
       Tests if an error is thrown when trying to merge a small array
     =#
-    k0::Array{Integer, 1} = collect(1:8);
-    j0::Array{Integer, 1} = collect(1:2);
+    k0::Array{Int64, 1} = collect(1:8);
+    j0::Array{Int64, 1} = collect(1:2);
     @test_throws BoundsError Prog1.mergeArrays(k0, j0, 8);
   end
 
   # Test for insert arrays 
   function insertArray()
-    k::Array{Integer, 1} = [1, 3, 4, 5, -1];
-    result:: Array{Integer, 1} = Prog1.insertArray(k, 5, 2);
+    k::Array{Int64, 1} = [1, 3, 4, 5, -1];
+    result:: Array{Int64, 1} = Prog1.insertArray(k, 5, 2);
     for i = 1:5
       @test result[i] === i;
     end
@@ -43,8 +62,8 @@ module Prog1Test
   function insert()
 
     # Test for basic Set that doesn't require merge
-    k1::Array{Integer, 1} = zeros(11);
-    l1::Array{Integer, 1} = [1, 3, 4, -1, -1];
+    k1::Array{Int64, 1} = zeros(11);
+    l1::Array{Int64, 1} = [1, 3, 4, -1, -1];
     set = Prog1.MySet(l1, k1, 3, 10);
     result = Prog1.insert(set, 2);
     for i = 1:4
@@ -53,8 +72,8 @@ module Prog1Test
     end
 
     # Test for Set that does require merge
-    k2::Array{Integer, 1} = [1, 5, 6, 7, -1, -1, -1, -1];
-    l2::Array{Integer, 1} = [2, 4, -1];
+    k2::Array{Int64, 1} = [1, 5, 6, 7, -1, -1, -1, -1];
+    l2::Array{Int64, 1} = [2, 4, -1];
     set = Prog1.MySet(l2, k2, 2, 4);
     result = Prog1.insert(set, 3);
     @test result.sizeSmall === 0;
@@ -64,8 +83,8 @@ module Prog1Test
   end
 
   function search()
-    k1::Array{Integer, 1} = [1, 5, 6, 7, -1, -1, -1, -1];
-    l1::Array{Integer, 1} = [2, 4, -1];
+    k1::Array{Int64, 1} = [1, 5, 6, 7, -1, -1, -1, -1];
+    l1::Array{Int64, 1} = [2, 4, -1];
     set = Prog1.MySet(l1, k1, 2, 4);
     @test Prog1.search(set, 7);
     @test !Prog1.search(set, 0);
@@ -78,6 +97,7 @@ module Prog1Test
 
   end
 
+  testSequence();
   insertArray();
   mergeArray();
   insert();
